e1a48e46e88356a336a252eb61d5bb68ba5ae80d
eleven <lyn19950705@163.com>
2019-05-28 23:16:33 +0800

    number2lcd done 1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,24 @@
 module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+	var lcd = [['._.','|.|','|_|'],
+		   ['...','..|','..|'],
+		   ['._.','._|','|_.'],
+		   ['._.','._|','._|'],
+		   ['...','|_|','..|'],
+		   ['._.','|_.','._|'],
+		   ['._.','|_.','|_|'],
+		   ['._.','..|','..|'],
+		   ['._.','|_|','|_|'],
+		   ['._.','|_|','..|']]
+    var result = ""
+	var nums = arguments[0].split('')
+	for (var i = 0; i <= 2; i++) {
+		for (var j = 0; j < nums.length-1; j++) {
+			result += lcd[nums[j]][i]
+			result += ' '
+        }	
+		result += lcd[nums[j]][i]
+		result += '\n'
+	}
+	console.log(result)
+	return result
 };
